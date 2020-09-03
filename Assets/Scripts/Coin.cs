220f36d55416ff9f8c6827ae7f676018ce35a1c7
Velepesh <62386083+Velepesh@users.noreply.github.com>
2020-09-03 16:21:25 +0300

    Added coin, wallet scripts

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.IO;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Wallet wallet))
+        {
+            wallet.AddCoin();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Wallet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Wallet : MonoBehaviour
+{
+    private int _coin;
+
+    public event UnityAction<int> CoinChanged;
+
+    public void AddCoin()
+    {
+        _coin++;
+
+        CoinChanged?.Invoke(_coin);
+    }
+}
