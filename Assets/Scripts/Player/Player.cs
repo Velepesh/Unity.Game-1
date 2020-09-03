b5d5346294056e0a29ab5d17c17f3e5cdce5ef31
Velepesh <62386083+Velepesh@users.noreply.github.com>
2020-09-03 16:20:21 +0300

    Add player scripts, camera scripts

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Player : MonoBehaviour
+{
+    readonly private Vector3 _slopeLimit = new Vector3(1f, 0f, 0f);
+
+    public UnityAction PlayerDied;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Vector3 collisionDirection = collision.GetContact(0).normal;
+
+        if(Mathf.Abs(collisionDirection.x) == _slopeLimit.x || collisionDirection.y < _slopeLimit.y)
+        {
+            PlayerDied?.Invoke();
+        }
+    }
+}
